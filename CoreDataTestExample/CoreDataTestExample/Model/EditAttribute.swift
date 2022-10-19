3a8195c82ef25bba40446ed7e1b5d6e1810cf092
jeonghyeon Hwang <hijihilin@gmail.com>
2022-10-20 05:04:40 +0900

    [feat] Add EditAttribute Enum

---
+++ CoreDataTestExample/CoreDataTestExample/Model/EditAttribute.swift
@@ -0,0 +1,13 @@
+//
+//  EditAttribute.swift
+//  CoreDataTestExample
+//
+//  Created by 황정현 on 2022/10/20.
+//
+
+import Foundation
+
+enum EditAttribute: String {
+    case gymName, gymVisitDate, problemLevel, isSucceeded, feedback, isFavorite
+    
+}
