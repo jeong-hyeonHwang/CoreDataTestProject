f9a0ea2654141c4df38a4d5983cdfdd31be029b2
jeonghyeon Hwang <hijihilin@gmail.com>
2022-10-19 22:16:42 +0900

    [feat] Add Core Data Test Buttons

--- CoreDataTestExample/CoreDataTestExample/ViewController.swift
+++ CoreDataTestExample/CoreDataTestExample/ViewController.swift
@@ -6,12 +6,52 @@
 //
 
 import UIKit
+import SnapKit
 
 class ViewController: UIViewController {
 
+    lazy var createDataButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("CREATE", for: .normal)
+        return button
+    }()
+    
+    lazy var readDataButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("READ", for: .normal)
+        return button
+    }()
+    
+    lazy var updateDataButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("UPDATE", for: .normal)
+        return button
+    }()
+    
+    lazy var deleteDataButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("UPDATE", for: .normal)
+        return button
+    }()
+    
+    lazy var deleteAllDataButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("UPDATE", for: .normal)
+        return button
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        [
+            createDataButton,
+            readDataButton,
+            updateDataButton,
+            deleteDataButton,
+            deleteAllDataButton
+        ].forEach({
+            view.addSubview($0)
+        })
     }
 
 
