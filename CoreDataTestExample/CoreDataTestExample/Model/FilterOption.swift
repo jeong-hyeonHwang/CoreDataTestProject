c58663a7d9e3465df82d281cf53f79501813f76f
jeonghyeon Hwang <hijihilin@gmail.com>
2022-10-22 07:18:38 +0900

    [feat] Add Filter & Sort enum

---
+++ CoreDataTestExample/CoreDataTestExample/Model/FilterOption.swift
@@ -0,0 +1,12 @@
+//
+//  FilterOption.swift
+//  CoreDataTestExample
+//
+//  Created by 황정현 on 2022/10/22.
+//
+
+import Foundation
+
+enum FilterOption {
+    case all, favorite, success, failure
+}
---
+++ CoreDataTestExample/CoreDataTestExample/Model/SortOption.swift
@@ -0,0 +1,12 @@
+//
+//  SortOption.swift
+//  CoreDataTestExample
+//
+//  Created by 황정현 on 2022/10/22.
+//
+
+import Foundation
+
+enum SortOption {
+    case gymName, gymVisitDate
+}
