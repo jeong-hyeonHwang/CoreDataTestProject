287b64cf790298b8c06ee815e83bc5d335860169
jeonghyeon Hwang <hijihilin@gmail.com>
2022-10-22 07:19:19 +0900

    [style] Remove Codes

--- CoreDataTestExample/CoreDataTestExample/Model/VideoInfoForDummy.swift
+++ CoreDataTestExample/CoreDataTestExample/Model/VideoInfoForDummy.swift
@@ -14,13 +14,4 @@ struct VideoInfoForDummy {
     var problemLevel: Int
     var isSucceeded: Bool
     var isFavorite: Bool
-    
-    func sortedStandard(_ standard: SortValue) -> String {
-        switch standard {
-        case .gymName:
-            return self.gymName
-        case .gymVisitDate:
-            return ""//self.gymVisitDate
-        }
-    }
 }
