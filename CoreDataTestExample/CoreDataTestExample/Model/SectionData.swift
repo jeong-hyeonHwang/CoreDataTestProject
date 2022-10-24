baff7cb33ba58c113eabe9158d416858861b3315
jeonghyeon Hwang <hijihilin@gmail.com>
2022-10-24 23:16:06 +0900

    [feat] Add SectionData Model

---
+++ CoreDataTestExample/CoreDataTestExample/Model/SectionData.swift
@@ -0,0 +1,16 @@
+//
+//  SectionData.swift
+//  CoreDataTestExample
+//
+//  Created by 황정현 on 2022/10/24.
+//
+
+import Foundation
+
+struct SectionData {
+    var orderOption: OrderOption
+    var sortOption: SortOption
+    var filterOption: FilterOption
+    var gymName: String
+    var gymVisitDate: Date
+}
